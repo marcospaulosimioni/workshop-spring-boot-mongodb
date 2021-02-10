388b5b4023f4d1fc9f78ae117fa9c191c46cc89b
Marcos Paulo Simioni <mpsimioni@hotmail.com>
2021-02-10 19:50:44 -0300

    Consulta com vários critérios

--- src/main/java/com/mpsimioni/workshopmongo/resources/util/URL.java
+++ src/main/java/com/mpsimioni/workshopmongo/resources/util/URL.java
@@ -2,15 +2,29 @@ package com.mpsimioni.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
 	public static String decodeParam(String text) {
 		try {
-		return URLDecoder.decode(text, "UTF-8");
-		}
-		catch (UnsupportedEncodingException e) {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
 	}
+
+	public static Date convertDate(String textDate, Date defaultDate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultDate;
+		}
+	}
+
 }
